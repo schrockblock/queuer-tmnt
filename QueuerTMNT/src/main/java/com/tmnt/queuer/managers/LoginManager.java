05f20777f9c6b50d1535bc70179c38ceaff5a7a1
Rahul <rn2@williams.edu>
2014-01-08 17:04:27 -0500

    added icons

---
+++ QueuerTMNT/src/main/java/com/tmnt/queuer/managers/LoginManager.java
@@ -0,0 +1,34 @@
+package com.tmnt.queuer.managers;
+
+import com.demo.queuer.Interfaces.LoginManagerCallback;
+
+/**
+ * Created by TMNT on 1/7/14.
+ */
+public class LoginManager {
+    private LoginManagerCallback callback;
+
+    public void setCallback(LoginManagerCallback callback) {
+        this.callback = callback;
+    }
+
+    public void login(String username, String password) throws Exception{
+        if (callback == null) throw new Exception("Must supply a LoginManagerCallback");
+        callback.startedRequest();
+        authenticate(username, password);
+    }
+
+    private void authenticate(String username, String password){
+
+    }
+
+    private void authenticatedSuccessfully() throws Exception{
+        if (callback == null) throw new Exception("Must supply a LoginManagerCallback");
+        callback.finishedRequest(true);
+    }
+
+    private void authenticatedUnsuccessfully() throws Exception{
+        if (callback == null) throw new Exception("Must supply a LoginManagerCallback");
+        callback.finishedRequest(false);
+    }
+}
