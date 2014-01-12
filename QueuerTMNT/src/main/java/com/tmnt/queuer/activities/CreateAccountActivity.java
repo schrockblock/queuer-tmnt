92efd02b59074b1f29638fe4c68c9a2f2a5211c6
Rahul <rn2@williams.edu>
2014-01-12 16:26:48 -0500

    added stuff to CreateAccount.java

--- QueuerTMNT/src/main/java/com/tmnt/queuer/activities/CreateAccountActivity.java
+++ QueuerTMNT/src/main/java/com/tmnt/queuer/activities/CreateAccountActivity.java
@@ -1,8 +1,21 @@
 package com.tmnt.queuer.activities;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.Button;
+
+import com.tmnt.queuer.R;
+
 /**
  * Created by rahul on 1/12/14.
  */
-public class CreateAccountActivity {
+public class CreateAccountActivity extends ActionBarActivity {
 
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_account);
+        Button loginButton = (Button)findViewById(R.id.btn_login);
+        Button createAccountButton = (Button)findViewById(R.id.btn_createAccount);
+    }
 }
